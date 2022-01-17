b8d25740761bcb45c234c3294b671cbd91ab8f5e
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-01-17 16:54:49 +0800

    update the comment become neater

--- permainan_cat_bola/PaintballGun.cs
+++ permainan_cat_bola/PaintballGun.cs
@@ -8,17 +8,20 @@ namespace permainan_cat_bola
     {
         //We’ll keep this constant public because
         //it’s going to be used by the Main method.
-        public const int MAGAZINE_SIZE = 16;
-
+        public const int MAGAZINE_SIZE = 16;  // We’ll keep this constant public because
+                                             //it’s going to be used by the Main method
         private int balls = 0;
         private int ballsLoaded = 0;
 
-        public int GetBallsLoaded()                   
-        { return this.ballsLoaded; }                 //<---  When the game needs to display the number of
-                                                    //<-- balls left and the number of balls loaded in the UI, it
-        public bool IsEmpty() { return this.ballsLoaded == 0; } //<-- // can call the GetBalls and GetBallsLoaded methods.
+        public int GetBallsLoaded()               //--------------------------------   
+        { return this.ballsLoaded; }             //When the game needs 
+                                                // to display the number of 
+        public bool IsEmpty()                   // balls left and the number       
+        { return this.ballsLoaded == 0; }       // of balls loaded in the UI, it
 
-        public int GetBalls() { return this.balls; }
+        public int GetBalls()                   // can call the GetBalls and
+        { return this.balls; }                  // GetBallsLoaded methods.
+                                                //---------------------------------
 
         public void SetBalls(int numberOfBalls)
         {
@@ -28,12 +31,12 @@ namespace permainan_cat_bola
         }                                   // allowing the game to set a positive number of balls.
                                             // Then it calls Reload to automatically reload the gun.
 
-        private void Reload()
-        {
-            if (this.balls > MAGAZINE_SIZE)
-                this.ballsLoaded = MAGAZINE_SIZE;
-            else
-                this.ballsLoaded = this.balls;
+        private void Reload()                       // The only way to reload the gun is to call the
+        {                                           // Reload method, which loads the gun with a
+            if (this.balls > MAGAZINE_SIZE)         // full magazine, or the remaining number of
+                this.ballsLoaded = MAGAZINE_SIZE;   // balls if there isn’t a full magazine’s worth.
+            else                                    // This keeps the balls and ballsLoaded fields
+                this.ballsLoaded = this.balls;      // from getting out of sync.
         }
 
 
@@ -41,9 +44,9 @@ namespace permainan_cat_bola
         {
 
             if (ballsLoaded == 0) return false;
-            this.ballsLoaded--;
-            this.balls--;
-            return true;
+            this.ballsLoaded--;                     // The Shoot method returns true
+            this.balls--;                           // and decrements the balls field if
+            return true;                            // the gun is loaded, or false if it isn’t.
         }
 
     }
